1bd3a00c2c13afb971bd42cbb2b0ce5a34b082c8
sephthompson <sephthompson@gmail.com>
2014-07-20 22:33:00 -0400

    update DBConnectionManager credentials

--- src/main/java/com/devcru/madnotes/DBConnectionManager.java
+++ src/main/java/com/devcru/madnotes/DBConnectionManager.java
@@ -5,10 +5,10 @@ public class DBConnectionManager {
 	// For now, we can just use this as a reference class for the URL.
 	
 	// localhost
-	private String url = "jdbc:postgresql://localhost:5432/madnotes?user=postgres&password=pass1234";
+	//private String url = "jdbc:postgresql://localhost:5432/madnotes?user=postgres&password=pass1234";
 	
 	//Heroku
-	//private String url = "jdbc:postgres://gogimhfcpjxqrl:8aYEwVsOfS27mVbmexusgRQfNI@ec2-54-83-33-14.compute-1.amazonaws.com:5432/d5n7019odngdsm";
+	private String url = "jdbc:postgres://gogimhfcpjxqrl:8aYEwVsOfS27mVbmexusgRQfNI@ec2-54-83-33-14.compute-1.amazonaws.com:5432/d5n7019odngdsm";
 
 	public String getUrl() {
 		return url;
