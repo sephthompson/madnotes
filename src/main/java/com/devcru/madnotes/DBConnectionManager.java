c402cfc4a0f237af40537dd657ad48e9eec63237
sephthompson <sephthompson@gmail.com>
2014-07-21 00:02:42 -0400

    DBConnectionManager credentials correction 2

--- src/main/java/com/devcru/madnotes/DBConnectionManager.java
+++ src/main/java/com/devcru/madnotes/DBConnectionManager.java
@@ -8,8 +8,9 @@ public class DBConnectionManager {
 	//private String url = "jdbc:postgresql://localhost:5432/madnotes?user=postgres&password=pass1234";
 	
 	//Heroku
-	private String url = "jdbc:postgresql://gogimhfcpjxqrl:8aYEwVsOfS27mVbmexusgRQfNI@ec2-54-83-33-14.compute-1.amazonaws.com:5432/d5n7019odngdsm";
+	private String url = "jdbc:postgresql://ec2-54-83-33-14.compute-1.amazonaws.com:5432/d5n7019odngdsm?user=gogimhfcpjxqrl&password=8aYEwVsOfS27mVbmexusgRQfNI";
 	
+	//Heroku example (from socialprofiles)
 	//private String url = "jdbc:postgresql://ec2-23-23-81-171.compute-1.amazonaws.com:5432/d3der2cpdnsd7k?user=oougodzmcwhapf&password=srdrgT5PV-VxBxlDGBPtzmFfsg";
 
 	public String getUrl() {
